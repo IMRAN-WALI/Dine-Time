48457d33eb04cdc93aaad3bee1f62647faad97fb
IMRAN WALI <imran.wali2001@gmail.com>
2026-03-07 15:15:22 +0500

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+    className="bg-red-500 h-30"
       style={{
         flex: 1,
         justifyContent: "center",
